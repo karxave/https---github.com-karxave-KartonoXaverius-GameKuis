13ccc1ce378feb45a638ab3fdf18c461cf021faa
karxave <30303126+karxave@users.noreply.github.com>
2024-05-02 00:53:44 +0700

    Message Console

--- Game Kuis PunyaKX/Assets/Scripts/UI_PoinJawaban.cs
+++ Game Kuis PunyaKX/Assets/Scripts/UI_PoinJawaban.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class UI_PoinJawaban : MonoBehaviour
 {
     [SerializeField]
+    private UI_PesanLevel _tempatPesan = null;
+
+    [SerializeField]
     private TextMeshProUGUI _teksJawaban = null;
 
     [SerializeField]
@@ -11,7 +14,8 @@ public class UI_PoinJawaban : MonoBehaviour
 
     public void PilihJawaban()
     {
-        Debug.Log($"{_teksJawaban.text} Adalah {_adalahBenar}");
+        //Debug.Log($"{_teksJawaban.text} Adalah {_adalahBenar}");
+        _tempatPesan.Pesan = $"{_teksJawaban.text} Adalah {_adalahBenar}";
     }
 
     public void SetJawaban(string teksJawaban, bool adalahBenar)
